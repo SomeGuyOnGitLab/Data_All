2b128abce8ac4f39da3fa421669f8bbe28de2ffd
SomeGuyOnGitLab <141304402+SomeGuyOnGitLab@users.noreply.github.com>
2024-11-17 22:16:28 +0530

    Delete QuickBooks vs TurboTax.sql

--- QuickBooks vs TurboTax.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-  SUM(CASE WHEN LOWER(product) LIKE 'turbotax%' THEN 1 ELSE 0 END) AS turbotax_total,
-  SUM(CASE WHEN LOWER(product) LIKE 'quickbooks%' THEN 1 ELSE 0 END) AS quickbooks_total
-FROM filed_taxes;
